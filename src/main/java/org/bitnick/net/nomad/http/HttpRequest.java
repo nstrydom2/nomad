8a6e68fcfc5f3b8ac17d1f9f10fb5d3b0540618d
Nick <nstrydom@gmail.com>
2021-04-26 19:22:45 -0400

    Added HttpRequest.java for static methods for requests

--- src/main/java/org/bitnick/net/nomad/http/HttpRequest.java
+++ src/main/java/org/bitnick/net/nomad/http/HttpRequest.java
@@ -1,4 +1,23 @@
 package org.bitnick.net.nomad.http;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class HttpRequest {
+    public static String get(URL url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        if (response.isSuccessful())
+            return response.body().string();
+
+        return null;
+    }
 }
