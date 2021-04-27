9a08674f428879155d0c6934ca7a5457b60ea85a
Nick <nstrydom@gmail.com>
2021-04-26 23:51:55 -0400

    Implemented WebScraper.java

--- src/main/java/org/bitnick/net/nomad/data/WebScraper.java
+++ src/main/java/org/bitnick/net/nomad/data/WebScraper.java
@@ -2,12 +2,23 @@ package org.bitnick.net.nomad.data;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class WebScraper {
-    public static void scrapeData(String rawPageSrc) {
+    public static List<Object> scrapeData(String rawPageSrc) {
         Document document = Jsoup.parse(rawPageSrc);
-        Elements elements = document.select("");
+        List<Element> elements = document.select("")
+                .stream()
+                .filter(Element::hasText)
+                .filter(e -> e.text().equalsIgnoreCase(""))
+                .collect(Collectors.toList());
 
+        if (!elements.isEmpty()) {
+            return null;
+        }
+
+        return null;
     }
 }
