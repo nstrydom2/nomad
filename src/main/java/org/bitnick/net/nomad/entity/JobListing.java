3d96e6d3712d745d809bd9dafc0adcdf089f7357
Nick <nstrydom@gmail.com>
2021-04-27 00:03:15 -0400

    Implemented POJO JobListing.java

--- src/main/java/org/bitnick/net/nomad/entity/JobListing.java
+++ src/main/java/org/bitnick/net/nomad/entity/JobListing.java
@@ -1,4 +1,33 @@
 package org.bitnick.net.nomad.entity;
 
+import java.net.URL;
+
 public class JobListing {
+    private String jobTitle;
+    private String listingTitle;
+    private String url;
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getListingTitle() {
+        return listingTitle;
+    }
+
+    public void setListingTitle(String listingTitle) {
+        this.listingTitle = listingTitle;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
