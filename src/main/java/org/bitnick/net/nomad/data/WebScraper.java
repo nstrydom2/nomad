c1d246a766f6da94c914828670597e64d09dce8c
Nick <nstrydom@gmail.com>
2021-04-27 16:00:18 -0400

    Updated for Webscraper.java

--- src/main/java/org/bitnick/net/nomad/data/WebScraper.java
+++ src/main/java/org/bitnick/net/nomad/data/WebScraper.java
@@ -1,10 +1,14 @@
 package org.bitnick.net.nomad.data;
 
 import org.bitnick.net.nomad.entity.JobListing;
+import org.bitnick.net.nomad.http.HttpRequest;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,8 +16,13 @@ import java.util.stream.Collectors;
 // TODO remove suppress warnings annotation
 @SuppressWarnings(value = "all")
 public class WebScraper {
-    public static List<JobListing> scrapeData(String rawPageSrc, String keyword, String cssSelector) {
-        Document document = Jsoup.parse(rawPageSrc);
+    public static List<JobListing> scrapeData(String keyword) throws MalformedURLException, IOException {
+        // TODO get correct url
+        URL url = new URL("https://test.change.me/");
+
+        // TODO add css selector for target page
+        String cssSelector = "[ADD RIGHT CSS SELECTER]";
+        Document document = Jsoup.parse(HttpRequest.get(url));
         List<Element> elements = document.select(cssSelector)
                 .stream()
                 .filter(Element::hasText)
